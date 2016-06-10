85ee85d8215fd481ac85202ff90ec59fd192d724
Devin McGloin <devin@devinmcgloin.com>
2016-06-10 01:43:48 -0700

    simple login template

--- src/views/publicView/login.go
+++ src/views/publicView/login.go
@@ -1,16 +1,12 @@
 package publicView
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/devinmcgloin/morph/src/views/common"
 	"github.com/julienschmidt/httprouter"
 )
 
 func UserLoginView(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
-	//TODO phase this out into a template
-	fmt.Fprintf(w, "<p><a href='/auth/twitter?provider=twitter'>Click to log in with twitter</a></p>")
-	fmt.Fprintf(w, "<p><a href='/auth/facebook?provider=facebook'>Click to log in with facebook</a></p>")
-	fmt.Fprintf(w, "<p><a href='/auth/gplus?provider=gplus'>Click to log in with gplus</a></p>")
+	common.RenderStatic(w, r, "templates/pages/uploadView.tmpl")
 }
