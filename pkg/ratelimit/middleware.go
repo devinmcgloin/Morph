083d6bbf038429f4db413cd1f218fa0c5d0f7d2b
Devin McGloin <devin@devinmcgloin.com>
2018-01-17 20:01:04 -0800

    updated tollbooth

--- pkg/ratelimit/middleware.go
+++ pkg/ratelimit/middleware.go
@@ -2,11 +2,15 @@ package ratelimit
 
 import (
 	"net/http"
+
 	"time"
 
 	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
 )
 
 func RateLimit(h http.Handler) http.Handler {
-	return tollbooth.LimitHandler(tollbooth.NewLimiter(5, time.Second, nil), h)
+	return tollbooth.LimitHandler(tollbooth.NewLimiter(5, &limiter.ExpirableOptions{
+		DefaultExpirationTTL: time.Second,
+	}), h)
 }
