8d11ad57a329257e3a9211fcfff80f60eccefb14
Devin McGloin <devin@devinmcgloin.com>
2018-02-03 18:14:28 -0800

    Removed permissions middleware for status endpoint

--- pkg/routes/status.go
+++ pkg/routes/status.go
@@ -2,8 +2,6 @@ package routes
 
 import (
 	"github.com/fokal/fokal/pkg/handler"
-	"github.com/fokal/fokal/pkg/model"
-	"github.com/fokal/fokal/pkg/security/permissions"
 	"github.com/fokal/fokal/pkg/status"
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
@@ -12,12 +10,7 @@ import (
 func RegisterStatusRoutes(state *handler.State, api *mux.Router, chain alice.Chain) {
 
 	head := api.Methods("HEAD").Subrouter()
-	head.Handle("/status",
-		chain.Append(
-			permissions.Middleware{State: state,
-				T:          permissions.CanView,
-				TargetType: model.Images,
-				M:          permissions.PermissionMiddle,
-			}.Handler).Then(handler.Handler{State: state, H: status.StatusHandler}))
+	head.Handle("/stauts",
+		chain.Then(handler.Handler{State: state, H: status.StatusHandler}))
 
 }
