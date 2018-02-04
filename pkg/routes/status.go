ca227f05b51fb05d4d44e311a0beb6f7e5525f5d
Devin McGloin <devin@devinmcgloin.com>
2018-02-03 18:21:07 -0800

    Wonder how that typo got in there

--- pkg/routes/status.go
+++ pkg/routes/status.go
@@ -10,7 +10,7 @@ import (
 func RegisterStatusRoutes(state *handler.State, api *mux.Router, chain alice.Chain) {
 
 	head := api.Methods("HEAD").Subrouter()
-	head.Handle("/stauts",
+	head.Handle("/status",
 		chain.Then(handler.Handler{State: state, H: status.StatusHandler}))
 
 }
