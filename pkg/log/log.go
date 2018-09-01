d90c912391de8d1aa64ab57cd42730117ad2085e
Devin McGloin <devin@devinmcgloin.com>
2018-09-01 22:23:27 +0100

    added logged in user id to default logs

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -8,7 +8,8 @@ import (
 
 func WithContext(ctx context.Context) *logrus.Entry {
 	return logrus.WithFields(logrus.Fields{
-		"request-id": ctx.Value(IDKey),
-		"request-ip": ctx.Value(IPKey),
+		"request-id":      ctx.Value(IDKey),
+		"request-ip":      ctx.Value(IPKey),
+		"request-user-id": ctx.Value(UserIDKey),
 	})
 }
