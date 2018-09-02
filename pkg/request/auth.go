53e21c3955770a3414da660d6370e8dd2c7e0ef4
Devin McGloin <devin@devinmcgloin.com>
2018-09-02 12:42:29 +0100

    Update auth.go

--- pkg/request/auth.go
+++ pkg/request/auth.go
@@ -6,12 +6,12 @@ import (
 	"github.com/mholt/binding"
 )
 
-type logrusinRequest struct {
+type LoginRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-func (cf *logrusinRequest) FieldMap(req *http.Request) binding.FieldMap {
+func (cf *LoginRequest) FieldMap(req *http.Request) binding.FieldMap {
 	return binding.FieldMap{
 		&cf.Username: "username",
 		&cf.Password: "password",
